d3100a977c0e1b4378e4f28c7f1c76f4d83687db
sijilsasidharan <sijilsasidharan@gmail.com>
2024-11-26 16:42:41 +0530

    Tailwind configs - 2

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Chefclaude from './apps/chef-claude/Chefclaude';
+import Login from './apps/login/Login';
+import UseRefHook from './hooks/useRef';
+function App() {
+
+  return (
+    <>
+      <Chefclaude></Chefclaude>
+      {/* <Login />
+      <UseRefHook /> */}
+    </>
+  )
+}
+
+export default App
