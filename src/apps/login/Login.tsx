def33b8fb59a12c570dc21f44c29ff9c57d7972c
sijilsasidharan <sijilsasidharan@gmail.com>
2024-11-27 18:03:59 +0530

    Login with other details

--- src/apps/login/Login.tsx
+++ src/apps/login/Login.tsx
@@ -6,11 +6,11 @@ const Login = () => {
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
-        const formDataJson: any = {};
-        for (const [key, value] of formData.entries()) {
-            formDataJson[key] = value;
-        }
-        console.log(formData, formDataJson);
+        const data = Object.fromEntries(formData.entries());
+        const dieteryRestrictions = formData.getAll('dieteryRestrictions');
+
+        
+        console.log({ ...data, dieteryRestrictions})
     }
   return (
     <form onSubmit={handleSubmit}>
@@ -20,6 +20,40 @@ const Login = () => {
         <label htmlFor='password'>Password </label>
         <input type='password' name='password' placeholder='Password'></input>
         <br />
+        <label htmlFor='description'>Description </label>
+        <textarea name='description' placeholder='Description'></textarea>
+        <br />
+        <fieldset>
+          <legend>Employement status</legend>
+          <label>
+            <input type="radio" name='employementStatus' value='unemployed' />
+            Unemployed
+          </label>
+          <label>
+            <input type="radio" name='employementStatus' value='part-time' />
+            Part time
+          </label>
+          <label>
+            <input type="radio" name='employementStatus' value='full-time' />
+            Full time
+          </label>
+        </fieldset>
+        <br />
+        <fieldset>
+          <legend>Dietery Restrictions</legend>
+          <label>
+            <input type="checkbox" name='dieteryRestrictions' value='kosher' />
+            Kosher
+          </label>
+          <label>
+            <input type="checkbox" name='dieteryRestrictions' value='vegan' />
+            Vegan
+          </label>
+          <label>
+            <input type="checkbox" name='dieteryRestrictions' value='glutan-free' />
+            Glutan-free
+          </label>
+        </fieldset>
         <button type='submit'>Login</button>
     </form>
   )
