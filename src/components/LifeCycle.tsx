a153786e04e17b69c783da71d0d01e8ffb62a553
sijilsasidharan <sijilsasidharan@gmail.com>
2024-11-30 00:35:28 +0530

    LifeCycle Update phase

--- src/components/LifeCycle.tsx
+++ src/components/LifeCycle.tsx
@@ -19,17 +19,41 @@ export class LifeCycle extends React.Component<LifeCycleProps, LifeCycleState> {
     static getDerivedStateFromProps(props: LifeCycleProps, state: LifeCycleState) {
         console.log('getDerivedStateFromProps', props, state);
         return {
-            counter: props.initialCount
+            counter: props.initialCount === state.counter ? props.initialCount : state.counter
         }
+    }
+
+    shouldComponentUpdate(nextProps: Readonly<LifeCycleProps>, nextState: Readonly<LifeCycleState>, nextContext: any): boolean {
+        if (nextState.counter % 2) {
+            return false;
+        }
+        return true;
+    }
+
+    handleRender = () => {
+        this.setState((prev) => ({
+            counter: prev.counter + 1
+        }));
+        console.log(this.state)
+    }
+
+    getSnapshotBeforeUpdate(prevProps: Readonly<LifeCycleProps>, prevState: Readonly<LifeCycleState>) {
+        console.log('getSnapshotBeforeUpdate', prevProps, prevState);
+        return null;
     }
 
     render(): React.ReactNode {
         console.log('render', this.state);
         return <>
             { this.state.counter }
+            <button className="border border-emerald-400 p-4 ml-4" onClick={this.handleRender}>Add</button>
         </>
     }
     componentDidMount(): void {
         console.log('componentDidMount', this.state)
+    }
+    
+    componentDidUpdate(): void {
+        console.log('componentDidUpdate', this.state)
     }
 }
